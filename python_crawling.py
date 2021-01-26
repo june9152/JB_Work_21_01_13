6133699a62ceeebfa00f5c48aa048af63796c94e
_SJB <june9152@gmail.com>
2021-01-26 13:55:10 +0900

    크롤링 블로그 접속 / 크롬드라이브 ㅡ88버전 업데이트
    클립보드 복사 후 텍스트 박스에 넣도록 코딩적용
    로그인 버튼 눌렀을 때 캡챠 안뜨도록 우회 코딩 적용
    블로그 로 한번에 가는 코드 적용
    경로 수정 필요.

--- python_crawling.py
+++ python_crawling.py
@@ -1,21 +1,50 @@
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import pyperclip
 import datetime
 import time
 
+
+
+
+def clipboard_input(user_xpath, user_input):
+        temp_user_input = pyperclip.paste()  # 사용자 클립보드를 따로 저장
+
+        pyperclip.copy(user_input)
+        driver.find_element_by_xpath(user_xpath).click()
+        ActionChains(driver).key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
+
+        pyperclip.copy(temp_user_input)  # 사용자 클립보드에 저장 된 내용을 다시 가져 옴
+        time.sleep(1)
+        
 login = {
     "id" : "93_11_23",
     "pw" : "wjaqls_23"
 }
 
-driver = webdriver.Chrome('./chromedriver')
+driver = webdriver.Chrome(r'C:\Users\손정빈\Desktop\python\JB_Work_21_01_13\chromedriver')
 driver.implicitly_wait(1)
 
 
+
+
 driver.get('https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com')
-driver.find_element_by_name('id').send_keys("93_11_23")
-driver.find_element_by_name('pw').send_keys("wjaqls_23")
-time.sleep(10);
-driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
+# driver.find_element_by_name('id').send_keys("93_11_23")
+# driver.find_element_by_name('pw').send_keys("wjaqls_23")
+# time.sleep(10);
+# driver.find_element_by_xpath('//*[@id="log.login"]').click()
+
+clipboard_input('//*[@id="id"]', login.get("id"))
+clipboard_input('//*[@id="pw"]', login.get("pw"))
+driver.find_element_by_xpath('//*[@id="log.login"]').click()
+time.sleep(1);
+# driver.find_element_by_xpath('/html/body/div/div/div[2]/div[7]/div/a').click()
+driver.get('https://blog.naver.com/93_11_23')
+driver.find_element_by_xpath('//*[@id="post-admin"]/a[1]').click()
+
+
+
 
 
 while True:
